58390114c312d0f7b4a4a0c9924a38fd7fbf28cd
Anji Reddy <anji.reddy@juspay.in>
2023-07-25 15:54:59 +0530

    Added Auto Completion mapper

--- src/components/CurlRequestExtractor.js
+++ src/components/CurlRequestExtractor.js
@@ -1,13 +1,15 @@
 import { useState, useEffect, useRef } from "react";
-import { parse_curl } from 'curl-parser';
-import $ from 'jquery';
+import { parse_curl } from "curl-parser";
+import $ from "jquery";
 import "../styles.css";
 import JSONEditor from "jsoneditor";
 import "jsoneditor/dist/jsoneditor.css";
-import mapFieldName from '../utils/search_utils'
+import mapFieldName from "../utils/search_utils";
+import jsonpath from "jsonpath";
 
 const CurlRequestExecutor = () => {
-  const [curlCommand, setCurlCommand] = useState(`curl --location --request POST 'https://api.shift4.com/charges' \
+  const [curlCommand, setCurlCommand] =
+    useState(`curl --location --request POST 'http://localhost:5050/https://api.shift4.com/charges' \
   --header 'X-router;' \
   --header 'Authorization: Basic c2tfdGVzdF93cjhMYjdqd1FNTEp1STJCMHBoSFJMVDQ6' \
   --header 'Content-Type: application/json' \
@@ -59,7 +61,7 @@ const CurlRequestExecutor = () => {
     "payment_id",
     "phone_number",
     "setup_future_usage",
-    "return_url"
+    "return_url",
   ];
   const [curlRequest, setCurlRequest] = useState({});
   const [originalData, setOriginalData] = useState(null);
@@ -67,20 +69,23 @@ const CurlRequestExecutor = () => {
   const jsonEditorRef = useRef(null);
   const requestEditorRef = useRef(null);
   const reponseEditorRef = useRef(null);
-  
+
   const [loading, setLoading] = useState(false);
   const options = {
     mode: "tree",
     modes: ["tree", "code"],
     autocomplete: {
-      filter: 'contain',
-      trigger: 'focus',
+      filter: "contain",
+      trigger: "focus",
       getOptions: function (text, path, input, editor) {
-        return suggestions.map((s) => '$'+s);
-      }
-    }
-  }
+        return suggestions.map((s) => "$" + s);
+      },
+    },
+  };
   let isLoaded = false;
+
+  //for options
+  const activationChar = "$";
 
   useEffect(() => {
     if (!isLoaded && jsonEditorRef.current) {
@@ -88,50 +93,44 @@ const CurlRequestExecutor = () => {
       const requestEditor = new JSONEditor(requestEditorRef.current, options);
       const responseEditor = new JSONEditor(reponseEditorRef.current, options);
       const jsonEditor = new JSONEditor(jsonEditorRef.current, options);
-      requestEditor.set({
-        "amount": 499,
-        "card": {
-            "number": "4012000100000007",
-            "expMonth": "03",
-            "expYear": "25",
-            "cvc": "123",
-            "cardholderName": "john"
-        },
-        "captured": true,
-        "currency": "EUR",
-        "description":"asdasd"
-    });
+
+      let rr = curlCommand
+        .replace(/\s*\\\s*/g, " ")
+        .replace(/\n/g, "")
+        .replace(/--data-raw|--data-urlencode/g, "-d");
+      setCurlCommand(rr);
+
+      var ss = parse_curl(rr);
+      setCurlRequest(ss);
+      requestEditor.set(mapFieldName(JSON.parse(ss.data.ascii)));
       requestEditor.expandAll();
       responseEditor.set({});
-      jsonEditor.set({
-        "Array": [1, 2, 3],
-        "asd": "asdasd",
-        "Boolean": true,
-        "user": "Dell",
-        "_id": 123,
-        "Object": { "a": "b", "c": "d" },
-        "abc": "Hello World"
-      }); // Set an initial empty JSON object
+      jsonEditor.set({}); // Set an initial empty JSON object
     }
   }, []);
 
   const updateCurlRequest = (request) => {
-    let ss = request.replace(/\s*\\\s*/g, ' ').replace(/\n/g, '').replace(/--data-raw|--data-urlencode/g, '-d');
+    let ss = request
+      .replace(/\s*\\\s*/g, " ")
+      .replace(/\n/g, "")
+      .replace(/--data-raw|--data-urlencode/g, "-d");
     setCurlCommand(request);
     try {
       const fetchRequest = parse_curl(ss);
       setCurlRequest(fetchRequest);
-      displayResponseFields(requestEditorRef, mapFieldName(JSON.parse(fetchRequest?.data?.ascii || "{}")));
-    }catch(e) {
-    }
-  }
+      displayResponseFields(
+        requestEditorRef,
+        mapFieldName(JSON.parse(fetchRequest?.data?.ascii || "{}"))
+      );
+    } catch (e) {}
+  };
 
   const isObject = (value) => {
-    return value && typeof value === 'object' && value.constructor === Object;
+    return value && typeof value === "object" && value.constructor === Object;
   };
 
   const isString = (value) => {
-    return typeof value === 'string' || value instanceof String;
+    return typeof value === "string" || value instanceof String;
   };
 
   const sendRequest = () => {
@@ -140,34 +139,36 @@ const CurlRequestExecutor = () => {
     const requestOptions = {
       method: curlRequest.method,
       headers: curlRequest.headers.reduce((acc, item) => {
-        const [key, value] = item.split(':').map(item => item.trim());
+        const [key, value] = item.split(":").map((item) => item.trim());
         acc[key] = value;
         return acc;
       }, {}),
       body: curlRequest.data.ascii,
     };
 
-    let url = '/cors/'+curlRequest.url;
+    let url = "/cors/" + curlRequest.url;
     let req_content = {
       type: requestOptions.method,
       url: url,
       headers: requestOptions.headers,
       data: requestOptions.body,
       success: (data) => {
-        console.log(data); setOriginalData(data);
+        console.log(data);
+        setOriginalData(data);
         displayResponseFields(reponseEditorRef, data);
-        displayResponseFields(jsonEditorRef, addFieldsToLeafNodes(data));
+        displayResponseFields2(jsonEditorRef, addFieldsToLeafNodes(data));
       },
-      error: (data) => { console.log(data) }
+      error: (data) => {
+        console.log(data);
+      },
     };
     $.ajax(url, req_content).always(() => setLoading(false));
-
   };
 
   function addFieldsToLeafNodes(jsonObj) {
     // Helper function to check if a value is an object (excluding arrays)
     function isObject(val) {
-      return typeof val === 'object' && !Array.isArray(val);
+      return typeof val === "object" && !Array.isArray(val);
     }
 
     // Recursive function to traverse the JSON object
@@ -212,12 +213,110 @@ const CurlRequestExecutor = () => {
     }
   }
 
+  function displayResponseFields2(jsonEditorRef, data, parentKey = null) {
+    const jsonString = JSON.stringify(data, null, 2);
+    const jsonData = JSON.parse(jsonString);
+
+    const options2 = {
+      autocomplete: {
+        confirmKeys: [39, 35, 9, 190], // Confirm Autocomplete Keys: [right, end, tab, '.']  // By default are only [right, end, tab]
+        caseSensitive: false,
+
+        getOptions: function (text, path, input, editor) {
+          if (!text.startsWith(activationChar) || input !== "value") return [];
+          let data = {};
+          let startFrom = 0;
+          const lastPoint = text.lastIndexOf(".");
+          const jsonObj = jsonData;
+          if (lastPoint > 0 && text.length > 1) {
+            data = jsonpath.query(
+              jsonObj,
+              "$." + text.substring(activationChar.length, lastPoint)
+            );
+            if (data.length > 0) {
+              data = data[0];
+            } else {
+              data = {};
+            }
+            // Indicate that autocompletion should start after the . (ignoring the first part)
+            startFrom = text.lastIndexOf(".") + 1;
+          } else {
+            data = jsonObj;
+          }
+
+          const optionsStr = YaskON.stringify(data, null, activationChar);
+          const options = optionsStr.split("\n");
+          return { startFrom: startFrom, options: options };
+        },
+      },
+    };
+
+    // helper function to auto complete paths of a JSON object
+    const YaskON = {
+      // Return first level json paths by the node 'o'
+      stringify: function (o, prefix, activationChar) {
+        prefix = prefix || "";
+        switch (typeof o) {
+          case "object":
+            let output = "";
+            if (Array.isArray(o)) {
+              o.forEach(
+                function (e, index) {
+                  output += activationChar + prefix + "[" + index + "]" + "\n";
+                }.bind(this)
+              );
+              return output;
+            }
+            output = "";
+            for (let k in o) {
+              if (o.hasOwnProperty(k)) {
+                if (prefix === "")
+                  output += this.stringify(o[k], k, activationChar);
+              }
+            }
+            if (prefix !== "") output += activationChar + prefix + "\n";
+            return output;
+          case "function":
+            return "";
+          default:
+            return prefix + "\n";
+        }
+      },
+    };
+
+    if (jsonEditorRef.current) {
+      jsonEditorRef.current.innerHTML = "";
+      if (jsonEditorRef.current && data) {
+        const jsonString = JSON.stringify(data, null, 2);
+        try {
+          const jsonData = JSON.parse(jsonString);
+          const jsonEditor = new JSONEditor(jsonEditorRef.current, options2);
+          jsonEditor.set({
+            status: "",
+            response: {
+              response_id: "",
+              card: {
+                number: "",
+              },
+            },
+          });
+          jsonEditor.expandAll();
+        } catch (error) {
+          console.error("Error parsing JSON:", error);
+        }
+      }
+    }
+  }
+
   const buildUpdatedResponse = (data, fields) => {
     if (isObject(data)) {
       const updatedResponse = {};
       for (const field of fields) {
         if (isObject(field.value)) {
-          updatedResponse[field.key] = buildUpdatedResponse(data[field.key], field.contentElement.props.value);
+          updatedResponse[field.key] = buildUpdatedResponse(
+            data[field.key],
+            field.contentElement.props.value
+          );
         } else {
           updatedResponse[field.key] = field.contentElement.props.value;
         }
@@ -251,18 +350,25 @@ const CurlRequestExecutor = () => {
         </div>
         <div className="request-body-section">
           <h3>Request Body Fields:</h3>
-          <div className="json-request-editor-container" ref={requestEditorRef}></div>
+          <div
+            className="json-request-editor-container"
+            ref={requestEditorRef}
+          ></div>
         </div>
 
         <div id="responseFieldsLeft" className="response-fields-left">
           <h3>Response</h3>
-          <div className="json-response-editor-container" ref={reponseEditorRef}></div>
+          <div
+            className="json-response-editor-container"
+            ref={reponseEditorRef}
+          ></div>
         </div>
 
         <div id="responseFieldsRight" className="response-fields-right">
           <h3>Response Fields Mapping</h3>
           <div className="json-editor-container" ref={jsonEditorRef}></div>
-        </div></div>
+        </div>
+      </div>
     </div>
   );
 };
