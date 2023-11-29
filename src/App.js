d9ad52e2998819abe2addac2830c159b1f459fc3
Kashif <kashif@protonmail.com>
2023-11-29 23:31:18 +0530

    fix: initialize curlRequest on app mount

--- src/App.js
+++ src/App.js
@@ -5,11 +5,17 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // userdef utils
 import { APP_CONTEXT, fetchItem } from 'utils/state';
-import { updateNestedJson } from 'utils/common';
+import {
+  addFieldsToNodes,
+  deepCopy,
+  mapFieldNames,
+  updateNestedJson,
+} from 'utils/common';
 
 // userdef UI components
 import Content from './components/composed/Content.jsx';
 import Header from './components/composed/Header.jsx';
+import { parse_curl } from 'curl-parser';
 
 function App() {
   const [appContext, setAppContext] = useRecoilState(APP_CONTEXT);
@@ -19,29 +25,45 @@ function App() {
    * Trigger - on every mount
    */
   useEffect(() => {
-    const appContext = fetchItem('app_context');
+    // Try to fetch from localStorage
+    let context = fetchItem('app_context');
+    context = typeof context === 'object' ? context : appContext;
     if (typeof appContext === 'object') {
       console.info('Found app_context in localStorage, updating app state');
-      setAppContext(appContext);
     }
+
+    // Initialize
+    const updatedFlows = deepCopy(context.flows);
+    Object.keys(context.flows).map((flow) => {
+      if (context.flows[flow].curlCommand !== '') {
+        const curlRequest = parse_curl(
+          context.flows[flow].curlCommand
+            .replace(/\s*\\\s*/g, ' ')
+            .replace(/\n/g, '')
+            .replace(/--data-raw|--data-urlencode/g, '-d')
+        );
+        const requestHeaderFields = curlRequest?.headers.reduce(
+          (result, item) => {
+            let header = item.split(':');
+            result[header[0]] = header[1].trim();
+            return result;
+          },
+          {}
+        );
+        const requestFields = JSON.parse(curlRequest?.data?.ascii || '{}');
+        updatedFlows[flow].curlRequest = curlRequest;
+        updatedFlows[flow].requestFields = {
+          value: requestFields,
+          mapping: addFieldsToNodes(mapFieldNames(requestFields)),
+        };
+        updatedFlows[flow].requestHeaderFields = {
+          value: requestHeaderFields,
+          mapping: addFieldsToNodes(mapFieldNames(requestHeaderFields)),
+        };
+      }
+    });
+    setAppContext({ ...context, flows: { ...context.flows, ...updatedFlows } });
   }, []);
-
-  const updateAppContextUsingPath = (path, update) => {
-    let updatedAppContext = updateNestedJson(
-      appContext,
-      path.split('.'),
-      update
-    );
-    setAppContext(updatedAppContext);
-  };
-
-  const updateAppContext = (updates) => {
-    const updatedAppContext = {
-      ...appContext,
-      ...updates,
-    };
-    setAppContext(updatedAppContext);
-  };
 
   return (
     <Router>
